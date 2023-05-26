553b4481b557c7a348ab8d70edf3041c4bf4d277
Brian <linbrian24@gmail.com>
2023-05-26 13:48:18 -0400

    Create basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
-export default function App() {}
+import Header from "./components/Header";
+import Intro from "./components/Intro";
+import Info from "./components/Info";
+import About from "./components/About";
+import Testimonial from "./components/Testimonial";
+import SignUp from "./components/SignUp";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Intro />
+      <Info />
+      <About />
+      <Testimonial />
+      <SignUp />
+      <Footer />
+    </div>
+  );
+}
