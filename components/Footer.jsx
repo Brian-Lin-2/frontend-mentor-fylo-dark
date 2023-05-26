b9a3dfb7e9578a5f958a1d97d9754632347a4f78
Brian <linbrian24@gmail.com>
2023-05-26 17:24:34 -0400

    Add basic HTML for SignUp and Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,1 +1,40 @@
-export default function Footer() {}
+export default function Footer() {
+  return (
+    <div>
+      <img src="./images/logo.svg" alt="" />
+
+      <div>
+        <img src="./images/icon-location.svg" alt="" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua
+        </p>
+      </div>
+
+      <div>
+        <img src="./images/icon-phone.svg" alt="" />
+        <p>+1-543-123-4567</p>
+      </div>
+
+      <div>
+        <img src="./images/icon-email.svg" alt="" />
+        <p>example@fylo.com</p>
+      </div>
+
+      <a>About Us</a>
+      <a>Jobs</a>
+      <a>Press</a>
+      <a>Blog</a>
+
+      <a>Contact</a>
+      <a>Terms</a>
+      <a>Privacy</a>
+
+      <div>
+        <img />
+        <img />
+        <img />
+      </div>
+    </div>
+  );
+}
--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -1,1 +1,16 @@
-export default function SignUp() {}
+export default function SignUp() {
+  return (
+    <div>
+      <h1>Get early access today</h1>
+      <p>
+        It only takes a minute to sign up and our free starter tier is extremely
+        generous. If you have any questions, our support team would be happy to
+        help you.
+      </p>
+      <form>
+        <input htmlFor="email" name="email" placeholder="example@email.com" />
+        <button>Get Started For Free</button>
+      </form>
+    </div>
+  );
+}
