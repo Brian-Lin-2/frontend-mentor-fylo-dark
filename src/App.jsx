d58fb7346caa3219f88f626a12aa05f660c1276c
Brian <linbrian24@gmail.com>
2023-05-27 19:01:25 -0400

    Fix white space error

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="w-mobile text-white text-sm font-open-sans">
+    <div className="text-white text-sm font-open-sans">
       <div className="bg-dark-blue-2 px-5">
         <Header />
         <Intro />
