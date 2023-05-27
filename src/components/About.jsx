e34b5a8ee1ccf014ab8cbf155ecec6e329e5e9a4
Brian <linbrian24@gmail.com>
2023-05-27 12:39:06 -0400

    Style About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,21 @@
 export default function About() {
   return (
-    <div>
-      <h1>Stay productive, wherever you are</h1>
+    <div className="flex flex-col gap-4 font-raleway mb-40 mx-3">
+      <img
+        className="mb-6"
+        src="./images/illustration-stay-productive.png"
+        alt="people working"
+      />
+      <h1 className="font-bold text-lg">Stay productive, wherever you are</h1>
       <p>
         Never let location be an issue when accessing your files. Fylo has you
         covered for all of your file storage needs.
-        <br />
+        <br /> <br />
         Securely share files and folders with friends, family and colleagues for
         live collaboration. No email attachments required.
       </p>
-      <a>
-        <p>See how Fylo works</p>
+      <a className="flex items-center gap-2 pb-0.5 border-b border-cyan w-fit">
+        <p className="text-cyan">See how Fylo works</p>
         <img src="./images/icon-arrow.svg" alt="" />
       </a>
     </div>
